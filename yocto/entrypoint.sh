6aface8fd9392fe7a6b4c5bec764f34cc4f55703
nbmoa <nbmoa@posteo.net>
2018-05-26 02:58:18 +0200

    Added docker

--- yocto/entrypoint.sh
+++ yocto/entrypoint.sh
@@ -8,6 +8,7 @@ USER_ID=${LOCAL_USER_ID:-1000}
 
 echo "Starting with UID : $USER_ID"
 useradd --shell /bin/bash -u $USER_ID -o -c "" -m user 2> /dev/null
+usermod -a -G docker user
 export HOME=/home/user
 
 exec /usr/local/bin/gosu user "$@"
